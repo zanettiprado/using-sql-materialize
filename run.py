87a65e0cec21e45150b102701716b036f644d45c
zanettiprado <>
2023-08-18 15:45:12 +0000

    push

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import os
 from taskmanager import app
 
-
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,3 +12,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+ else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+ else:
+    uri = os.environ.get("DATABASE_URL")
+     if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
